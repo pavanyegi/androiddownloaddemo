3849ed18a920e6ea54c3b8f7fea1aead657a1bcd
pavan <pavan.intelmor@gmail.com>
2018-10-17 16:08:55 +0530

    testing commit

--- app/src/main/java/com/androiddownloaddemo/CheckForSDCard.java
+++ app/src/main/java/com/androiddownloaddemo/CheckForSDCard.java
@@ -3,9 +3,10 @@ package com.androiddownloaddemo;
 import android.os.Environment;
 
 public class CheckForSDCard {
-    //Check If SD Card is present or not method
-    public boolean isSDCardPresent() {
-        if (Environment.getExternalStorageState().equals(
+    //Check If SD Card is present or not methodss
+    public boolean isSDCardPresent()
+    {
+        if(Environment.getExternalStorageState().equals(
 
                 Environment.MEDIA_MOUNTED)) {
             return true;
