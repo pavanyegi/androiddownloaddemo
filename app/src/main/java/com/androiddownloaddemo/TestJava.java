42110c9ad127e2f52df0a4d56216a4f85ca423e3
pavan <pavan.intelmor@gmail.com>
2018-10-20 16:05:23 +0530

    second commit

---
+++ app/src/main/java/com/androiddownloaddemo/TestJava.java
@@ -0,0 +1,13 @@
+package com.androiddownloaddemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestJava extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+}
